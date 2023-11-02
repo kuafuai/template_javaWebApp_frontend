40d35e905d87ae635c416cb3e7dee68c2a661041
charging-kuafuai <charging@126.com>
2023-11-02 15:44:46 +0800

    资金计划功能： 我要设计一款软件，当我输入客户需求的产品型号及需求时间后，系统能够从后台的供应商渠道中帮我选择出所有交付时间在需求时间内的采购方案，并按支出金额的数额大小进行排序。 给我提供一个添加和修改供应商信息的页面，后台的供应商信息包括：产品型号，交付时间，产品价格，可交付数量，交易对手方，预付款比例等。

--- api.js
+++ api.js
@@ -1,69 +1,67 @@
 const express = require('express');
 const router = express.Router();
 
-// Import the necessary models for database operations
-const PurchasePlan = require('../models/purchasePlan');
-const Supplier = require('../models/supplier');
+// Dummy data for testing
+let purchasePlans = [
+  { supplier: 'Supplier A', expenditure: 100 },
+  { supplier: 'Supplier B', expenditure: 200 },
+  { supplier: 'Supplier C', expenditure: 150 }
+];
 
 // GET /api/suppliers
-router.get('/suppliers', async (req, res) => {
-  try {
-    // Retrieve product model and demand time from request query parameters
-    const { productModel, demandTime } = req.query;
+router.get('/suppliers', (req, res) => {
+  // Retrieve product model and demand time from request query parameters
+  const { productModel, demandTime } = req.query;
   
-    // Retrieve and filter purchase plans based on product model and demand time
-    const purchasePlans = await PurchasePlan.find({ productModel, demandTime }).sort({ expenditure: 1 });
-  
-    res.json({ purchasePlans });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+  // Filter purchase plans based on product model and demand time
+  let filteredPlans = purchasePlans;
+  if (productModel) {
+    filteredPlans = filteredPlans.filter(plan => plan.productModel === productModel);
   }
+  if (demandTime) {
+    filteredPlans = filteredPlans.filter(plan => plan.demandTime === demandTime);
+  }
+  
+  // Sort purchase plans by expenditure amount
+  filteredPlans.sort((a, b) => a.expenditure - b.expenditure);
+  
+  res.json({ purchasePlans: filteredPlans });
 });
 
 // POST /api/suppliers
-router.post('/suppliers', async (req, res) => {
-  try {
-    // Retrieve supplier information from request body
-    const { name, type } = req.body;
+router.post('/suppliers', (req, res) => {
+  // Retrieve supplier information from request body
+  const { name, type } = req.body;
   
-    // Create a new supplier in the database
-    const supplier = await Supplier.create({ name, type });
+  // Add supplier to the database
+  const supplier = { name, type };
+  // TODO: Implement logic to add supplier to the database
   
-    res.json({ supplier });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
+  res.json({ supplier });
 });
 
 // PUT /api/suppliers/:supplierId
-router.put('/suppliers/:supplierId', async (req, res) => {
-  try {
-    // Retrieve supplier ID and updated information from request parameters and body
-    const { supplierId } = req.params;
-    const { name, type } = req.body;
+router.put('/suppliers/:supplierId', (req, res) => {
+  // Retrieve supplier ID and updated information from request parameters and body
+  const { supplierId } = req.params;
+  const { name, type } = req.body;
   
-    // Update the supplier in the database
-    const supplier = await Supplier.findByIdAndUpdate(supplierId, { name, type }, { new: true });
+  // Update supplier in the database
+  const supplier = { id: supplierId, name, type };
+  // TODO: Implement logic to update supplier in the database
   
-    res.json({ supplier });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
+  res.json({ supplier });
 });
 
 // DELETE /api/suppliers/:supplierId
-router.delete('/suppliers/:supplierId', async (req, res) => {
-  try {
-    // Retrieve supplier ID from request parameters
-    const { supplierId } = req.params;
+router.delete('/suppliers/:supplierId', (req, res) => {
+  // Retrieve supplier ID from request parameters
+  const { supplierId } = req.params;
   
-    // Delete the supplier from the database
-    await Supplier.findByIdAndDelete(supplierId);
+  // Delete supplier from the database
+  // TODO: Implement logic to delete supplier from the database
   
-    res.sendStatus(200);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
+  res.sendStatus(200);
 });
 
 module.exports = router;
